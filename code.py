168776edc1d274416400070c3692ff7d1ea8875c
Wilson Dias Debus <wilsonddebus@gmail.com>
2026-03-05 08:42:35 -0300

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 # Criar uma IA 
 
 # Passo a passo do projeto 
-# Passo 0: entender o desafio da empresa 
+
 # Passo 1: Importar a base de dados 
 # pip install scikit-learn
 
@@ -37,13 +37,13 @@ x = tabela.drop(["score_credito", "id_cliente"], axis=1)   #pega todas as coluna
 # x_teste // y_teste
 
 from sklearn.model_selection import train_test_split 
-x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)   #fazendo o treino e os testes da IA(sempre nessa ordem) e o percentual de teste e treino
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)   #fazendo o treino e os testes da IA e o percentual de teste e treino
 
-# Passo 3: Modelo de previsão ou modelo de IA -> BOA, OK, RUIM
+# Passo 3: Modelo de previsão ou modelo de IA 
 
-# Modelos: (existem infinitos modelos mas vou usar esses)
+# Modelos:
 # Arvore de Decisão -> RandomForest
-# KNN -> Nearest Neighbors 
+# KNN -> Nearest Neighbors
 
 y = tabela["score_credito"]
 x = tabela.drop(["score_credito", "id_cliente"], axis=1)
@@ -51,25 +51,25 @@ x = tabela.drop(["score_credito", "id_cliente"], axis=1)
 from sklearn.model_selection import train_test_split 
 x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)
 
-# passo 1: Importar a IA (modelo)
+# Importar a IA (modelo)
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.neighbors import KNeighborsClassifier 
 
-# passo 2: Criar a IA (modelo)
+# Criar a IA (modelo)
 modelo_arvoredecisao = RandomForestClassifier()
 modelo_knn = KNeighborsClassifier()
 
-# passo 3: Treinar a IA (modelo)
+# Treinar a IA (modelo)
 modelo_arvoredecisao.fit(x_treino, y_treino)
 modelo_knn.fit(x_treino, y_treino)
 
-# Passo 4: Avaliar e escolher o melhor modelo 
+# Avalio e escolho o melhor modelo 
 
 # previsao 
 previsao_arvore = modelo_arvoredecisao.predict(x_teste)
 previsao_knn = modelo_knn.predict(x_teste)
 
-# comparar a previsao com o y_teste
+# comparo a previsao com o y_teste
 from sklearn.metrics import accuracy_score
 
 print("Arvore de Decisão: ")
@@ -77,14 +77,12 @@ print(accuracy_score(y_teste, previsao_arvore))
 print("KNN: ")
 print(accuracy_score(y_teste, previsao_knn))
 
-# Passo 5: Fazer novas previsões 
-
 # melhor modelo = Arvore de decisão
 
-# importar novos clientes 
+# importo novos clientes 
 novos_clientes = pd.read_csv("novos_clientes.csv")
 
-# não usa o fit antes do transform, pois se não ia criar outros numeroa para cada str
+# não uso o fit antes do transform, pois se não ia criar outros numeroa para cada str
 novos_clientes["profissao"] = codificador_profissao.transform(novos_clientes["profissao"])  #aplicando o codificador na coluna especifica 
 
 novos_clientes["mix_credito"] = codificador_mix.transform(novos_clientes["mix_credito"])
